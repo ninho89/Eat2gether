79a066a344407c78559051c8f9c14111170c45b9
Filippo Aresu <fili.aresu@gmail.com>
2015-03-17 14:45:07 +0100

    create project xcode

---
+++ EatTogether/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EatTogether
+//
+//  Created by Filippo Aresu on 17/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EatTogether/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EatTogether
+//
+//  Created by Filippo Aresu on 17/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
