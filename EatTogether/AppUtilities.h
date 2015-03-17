5c2637639ed3a2e52a397e75a6840be229b3660a
Filippo Aresu <fili.aresu@gmail.com>
2015-03-17 15:00:17 +0100

    add utilities class with application_id adn client_key of parse aplication

---
+++ EatTogether/AppUtilities.h
@@ -0,0 +1,19 @@
+//
+//  AppUtilities.h
+//  ParseApp
+//
+//  Created by Filippo Aresu on 17/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#ifndef ParseApp_AppUtilities_h
+#define ParseApp_AppUtilities_h
+
+
+#define APPLICATION_ID 	@"kZNlKv6ctrkEw5zYSm19JUr0cMft2RkHVj8m1aSo"
+#define CLIENT_KEY 		@"UT0TWIxZyprtsHGk6EbaR6ig80BUQltJvXghLhQ8"
+
+
+
+
+#endif
