5a3135c6b0d04f87781a9913ba4ad60f9e221269
Filippo Aresu <fili.aresu@gmail.com>
2015-03-17 15:01:32 +0100

    add constants class with name's constants of parse table

---
+++ EatTogether/Constants.h
@@ -0,0 +1,27 @@
+//
+//  Constants.h
+//  EatTogetherPrueba
+//
+//  Created by Filippo Aresu on 16/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString * const kCityTableParse;
+extern NSString * const kCityIdParse;
+extern NSString * const kCityNameParse;
+extern NSString * const kCityCountryParse;
+extern NSString * const kCityLocationIdParse;
+extern NSString * const kCityPictureParse;
+
+extern NSString * const kLocationTableParse;
+extern NSString * const kLocationIdParse;
+extern NSString * const kLocationCityIdParse;
+extern NSString * const kLocationLatitude;
+extern NSString * const kLocationLongitude;
+
+
+@end
---
+++ EatTogether/Constants.m
@@ -0,0 +1,27 @@
+//
+//  Constants.m
+//  EatTogetherPrueba
+//
+//  Created by Filippo Aresu on 16/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+NSString * const kCityTableParse = @"City";
+NSString * const kCityIdParse = @"cityId";
+NSString * const kCityNameParse = @"cityName";
+NSString * const kCityCountryParse = @"cityCountry";
+NSString * const kCityLocationIdParse = @"locationId";
+NSString * const kCityPictureParse = @"cityPicture";
+
+
+NSString * const kLocationTableParse = @"Location";
+NSString * const kLocationIdParse = @"locationId";
+NSString * const kLocationCityIdParse = @"cityId";
+NSString * const kLocationLatitude = @"locationLatitude";
+NSString * const kLocationLongitude = @"locationLongitude";
+
+@end
